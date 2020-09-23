23a9b3c04243aeb7c74185e6abfa93e5ef2050c8
kingauther33 <64845909+kingauther33@users.noreply.github.com>
2020-09-23 08:29:24 +0700

    a

--- Project_PrimeFitness/js/mainjs.js
+++ Project_PrimeFitness/js/mainjs.js
@@ -6,7 +6,16 @@ $(document).ready(function() {
         $(".loader").fadeOut();
         $("#preloader").delay(200).fadeOut('slow');
 
-        //=============  Add Wow =============//
+//=============  Add Wow =============//
         new WOW().init();
 });
 
+
+//=============  Gallery Page =============//
+
+$(document).ready(function() {
+        $('.image-popup').magnificPopup({type:'image'});
+});
+
+
+//=============  Gallery Page =============//
